643b9bb3dc09e69376cab345f6f5f415291ef92e
mrbergis <mrbergis@mail.ru>
2023-04-15 18:42:41 +0300

    12. Creating a Top Down MP Shooter Player Rotation & Animation Logics

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -15,15 +15,21 @@ public class PlayerLogic : MonoBehaviour
     
     const float MOVEMENT_SPEED = 5.0f;
     
+    Animator _animator;
+    
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
     
     void Update()
     {
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
+
+        RotateTowardsMouseCursor();
+        UpdateMovementAnimation();
     }
 
     private void FixedUpdate()
@@ -34,6 +40,31 @@ public class PlayerLogic : MonoBehaviour
         if(_characterController)
         {
             _characterController.Move(_horizontalMovement + _verticalMovement);
+        }
+    }
+    
+    private  void RotateTowardsMouseCursor()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 direction = mousePos - objectPos;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle + 90.0f, Vector3.up);
+    }
+    
+    void UpdateMovementAnimation()
+    {
+        Vector3 normalizedVector = Vector3.Normalize(Vector3.forward 
+            * _verticalInput + Vector3.right 
+            * _horizontalInput);
+        float dotProduct = Vector3.Dot(transform.forward, normalizedVector);
+        float dotProductStrafe = Vector3.Dot(transform.right, normalizedVector);
+
+        if(_animator)
+        {
+            _animator.SetFloat("VerticalInput", dotProduct);
+            _animator.SetFloat("HorizontalInput", dotProductStrafe);
         }
     }
 }
