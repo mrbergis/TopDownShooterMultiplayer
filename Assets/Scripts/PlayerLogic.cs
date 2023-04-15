5a8c68c8458a791f92f5731f6f769534a23dc978
mrbergis <mrbergis@mail.ru>
2023-04-15 14:07:34 +0300

    11. Creating a Top Down MP Shooter Scene & Player Setup -создал проект -setting camera Orthographic -import model -setting character controller -crest script PlayerController -mov horizontal vertical

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    CharacterController _characterController;
+
+    float _horizontalInput;
+    float _verticalInput;
+    
+    Vector3 _horizontalMovement;
+    Vector3 _verticalMovement;
+    
+    const float MOVEMENT_SPEED = 5.0f;
+    
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _horizontalMovement = Vector3.right * _horizontalInput * MOVEMENT_SPEED * Time.deltaTime;
+        _verticalMovement = Vector3.forward * _verticalInput * MOVEMENT_SPEED * Time.deltaTime;
+
+        if(_characterController)
+        {
+            _characterController.Move(_horizontalMovement + _verticalMovement);
+        }
+    }
+}
